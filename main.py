8cf3e66e18b926802bed2f4b11dfd524c7128721
Roj A <roja@MacBook-Air-2.local>
2024-11-14 17:50:02 +0000

    commit 6

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 import qrcode
+import sqlite3
+import io
+from smtplib import SMTP
 
-customer = input("please enter a url you would like to add")
-img = qrcode.make(customer)
-type(img)
-img.save("some_file.png")
+
+
+with sqlite3.connect('my_database.db') as connection:
+    customer_websearch = input("please enter a url you would like to add")
+    customer_name = input("please enter your name")
+    customer_email =input("please enter your email")
+    
+
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS QRCodeinfo (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        link TEXT,
+        img BLOB 
+    );
+    
+
+    ''')
+    # Binary large object data type.
+
+    
+    img = qrcode.make(customer_websearch)
+    img_buffer=io.BytesIO() #define img_buffer as a BYTESIO object
+    img.save(img_buffer,format="PNG")
+    img_binary = img_buffer.getvalue() # retrieves all the contents of the img buffer as a byte object representing in a image data in binary format
+
+    insert_query = '''
+    INSERT INTO QRCodeinfo (name,link,img)
+    VALUES (?,?,?);
+    '''
+    cursor.execute(insert_query,(customer_name,customer_websearch,img_binary))
+
+connection.commit()
+
+
+
 
